cae01573898d62fb082994e2ae6bced641724db9
AdayPA <dyto_aday@hotmail.com>
2021-03-10 10:53:28 +0000

    uc pila

--- cpu/uc.v
+++ cpu/uc.v
@@ -7,21 +7,25 @@ casez (opcode)
 			op_alu = opcode[4:2];
 			wez = 1; s_inm = 0;
 			we3 = 1; s_inc = 1;
-			s_pila = 0;
+			s_pila = 0; push = 0;
+			pop = 0;
 		end
 	6'b100000:   // carga inmediata
 		begin
 			s_inm = 1; we3 = 1;
 			s_inc = 1; s_pila = 0;
+			push = 0; pop = 0;
 		end
 	6'b100001:  // salto
 		begin
 			s_inc = 0; s_pila = 0;
+			push = 0; pop = 0;
 		end
 	6'b100010:  // salto si z
 		begin
 			s_pila = 0;
 			we3 = 0; wez = 0;
+			push = 0; pop = 0;
 			// if ( z == 1b'1 ) begin
 			if ( z == 1 ) begin
 				s_inc = 0;
@@ -33,6 +37,7 @@ casez (opcode)
 		begin
 			s_pila = 0;
 			we3 = 0; wez = 0;
+			push = 0; pop = 0;
 			if ( z == 0 ) begin
 				s_inc = 0;
 			end else begin
@@ -41,13 +46,15 @@ casez (opcode)
 		end
 	6'b100100: // push pila
 		begin
-			push = 1;
-			s_pila = 1;
+			push = 1; pop = 0;
+			s_pila = 0;
+			we3 = 0; wez = 0;
 		end
 	6'b100101: // pop pila
 		begin
-			pop = 1;
+			pop = 1; push = 0;
 			s_pila = 1;
+			we3 = 0; wez = 0;
 		end
 
 default: ;
